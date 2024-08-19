555491c4a2faa3845ae83c5e28d6455873559a1d
ExuFar <farihinsaleh@gmail.com>
2024-08-20 02:49:38 +0800

    Try function on raspberry pi

---
+++ RPI-I2C-LCD/led.py
@@ -0,0 +1,40 @@
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+
+import RPi.GPIO as GPIO
+import time
+
+I2C_ADDR = 0x27
+I2C_NUM_ROWS = 4
+I2C_NUM_COLS = 20
+lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+GPIO.setmode(GPIO.BCM)
+sensor_pin = 17
+GPIO.setup(sensor_pin, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(sensor_pin, GPIO.HIGH)
+        print("IR LED ON")
+        time.sleep(1)
+        
+        GPIO.output(sensor_pin, GPIO.LOW)
+        print("IR LED OFF")
+        time.sleep(1)
+        
+except KeyboardInterrupt:
+    print("Program stopped")
+    
+finally:
+    GPIO.cleanup()
+    
+ 
+#lcd.move_to(1,0)
+#lcd.putstr("SELAMAT DATANG  KE\n")
+#lcd.move_to(5,1)
+#lcd.putstr("SECTION 40\n")
+#lcd.move_to(1,2)
+#lcd.putstr("THIS IS PARKIT CAR\n")
+#lcd.move_to(3,3)
+#lcd.putstr("PARKING SYSTEM")
