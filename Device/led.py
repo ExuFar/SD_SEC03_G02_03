628f49a947b8d8b90ce98cb276b718ca1e2a963b
ExuFar <farihinsaleh@gmail.com>
2024-10-29 01:30:24 +0800

    update led.py

--- Device/led.py
+++ Device/led.py
@@ -14,11 +14,11 @@ GREEN_LED_PARKING1_PIN = 5       # Green LED for parking slot 1 (available)
 RED_LED_PARKING1_PIN = 6         # Red LED for parking slot 1 (occupied)
 
 # LEDs for parking slot 2
-GREEN_LED_PARKING2_PIN = 12      # Green LED for parking slot 2 (available)
-RED_LED_PARKING2_PIN = 16        # Red LED for parking slot 2 (occupied)
+GREEN_LED_PARKING2_PIN = 13      # Green LED for parking slot 2 (available)
+RED_LED_PARKING2_PIN = 19        # Red LED for parking slot 2 (occupied)
 
 # LEDs for parking slot 3
-GREEN_LED_PARKING3_PIN = 20      # Green LED for parking slot 3 (available)
+GREEN_LED_PARKING3_PIN = 26      # Green LED for parking slot 3 (available)
 RED_LED_PARKING3_PIN = 21        # Red LED for parking slot 3 (occupied)
 
 
@@ -27,13 +27,15 @@ TOTAL_SLOTS = 3  # Modify based on the number of slots
 
 # LCD Definitions (assuming you use I2C to control the LCD)
 I2C_ADDR = 0x27          # I2C address of the LCD
-I2C_NUM_ROWS = 4         # Number of rows on the LCD
-I2C_NUM_COLS = 20        # Number of columns on the LCD
+I2C_NUM_ROWS = 20         # Number of rows on the LCD
+I2C_NUM_COLS = 4        # Number of columns on the LCD
 lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_COLS, I2C_NUM_ROWS)
 
 # Setup GPIO mode
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(IR_PARKING1_PIN, GPIO.IN)
+GPIO.setup(IR_PARKING2_PIN, GPIO.IN)
+GPIO.setup(IR_PARKING3_PIN, GPIO.IN)
 GPIO.setup(GREEN_LED_PARKING1_PIN, GPIO.OUT)
 GPIO.setup(RED_LED_PARKING1_PIN, GPIO.OUT)
 GPIO.setup(GREEN_LED_PARKING2_PIN, GPIO.OUT)
