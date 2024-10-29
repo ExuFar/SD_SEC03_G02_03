32f7b38327cd48ae49fb6e100bda0add8fcbd44e
ExuFar <farihinsaleh@gmail.com>
2024-10-29 20:47:03 +0800

    Connect to Firebase

--- Device/Function.py
+++ Device/Function.py
@@ -4,6 +4,15 @@ from i2c_lcd import I2cLcd
 import RPi.GPIO as GPIO 
 import time
 
+import firebase_admin
+from firebase_admin import credentials, db
+
+# Initialize Firebase
+cred = credentials.Certificate("D:/farihin/Desktop/SD/serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://park-i-t-c4s9f4-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
 
 # GPIO Pin Definitions
 IR_PARKING1_PIN = 23    # IR sensor for parking slot 1
@@ -12,7 +21,7 @@ IR_PARKING3_PIN = 25    # IR sensor for parking slot 3
 IR_ENTRY_PIN = 27       # IR sensor for car entry
 IR_EXIT_PIN = 22        # IR sensor for car exit
 SERVO_ENTRY_PIN = 17    # GPIO pin for entry servo motor
-SERVO_EXIT_PIN = 18     # GPIO pin for exit servo motor
+SERVO_EXIT_PIN = 18     # GPIO pin for exit servo motor            
 
 # LEDs for parking slot 1
 GREEN_LED_PARKING1_PIN = 5       # Green LED for parking slot 1 (available)
@@ -25,6 +34,7 @@ RED_LED_PARKING2_PIN = 19        # Red LED for parking slot 2 (occupied)
 # LEDs for parking slot 3
 GREEN_LED_PARKING3_PIN = 26      # Green LED for parking slot 3 (available)
 RED_LED_PARKING3_PIN = 21        # Red LED for parking slot 3 (occupied)
+
 
 # Total number of parking slots
 TOTAL_SLOTS = 3  # Modify based on the number of slots
@@ -42,17 +52,18 @@ GPIO.setwarnings(False)
 GPIO.setup(IR_PARKING1_PIN, GPIO.IN)
 GPIO.setup(IR_PARKING2_PIN, GPIO.IN)
 GPIO.setup(IR_PARKING3_PIN, GPIO.IN)
+
 GPIO.setup(SERVO_ENTRY_PIN, GPIO.OUT)
 GPIO.setup(SERVO_EXIT_PIN, GPIO.OUT)
 GPIO.setup(IR_ENTRY_PIN, GPIO.IN)
 GPIO.setup(IR_EXIT_PIN, GPIO.IN)
+
 GPIO.setup(GREEN_LED_PARKING1_PIN, GPIO.OUT)
 GPIO.setup(RED_LED_PARKING1_PIN, GPIO.OUT)
 GPIO.setup(GREEN_LED_PARKING2_PIN, GPIO.OUT)
 GPIO.setup(RED_LED_PARKING2_PIN, GPIO.OUT)
 GPIO.setup(GREEN_LED_PARKING3_PIN, GPIO.OUT)
 GPIO.setup(RED_LED_PARKING3_PIN, GPIO.OUT)
-
 
 
 # Set the PWM signal for the servo motors (50 Hz frequency)
@@ -167,6 +178,8 @@ try:
             update_led3(occupied=False)  # No car, mark as empty   
 
 
+        available_slots = count_available_slots()
+        update_lcd(available_slots)
         time.sleep(0.1)  # Short delay to avoid excessive CPU usage
 
 finally:
