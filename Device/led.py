fc0386c3590a655a0bed0555bc3c3a74b3327a75
ExuFar <farihinsaleh@gmail.com>
2024-11-05 01:25:43 +0800

    Send Available Slot to firebase

--- Device/led.py
+++ Device/led.py
@@ -3,23 +3,22 @@ from i2c_lcd import I2cLcd
 import RPi.GPIO as GPIO
 import time
 
+import firebase_admin
+from firebase_admin import credentials, db
+
+
+# Firebase setup
+cred = credentials.Certificate("/home/farihin/Desktop/SD/serviceAccountKey.json")  
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://park-i-t-c4s9f4-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+ref = db.reference('Total_Available_slots')
 
 # GPIO Pin Definitions
 IR_PARKING1_PIN = 23    # IR sensor for parking slot 1
 IR_PARKING2_PIN = 24    # IR sensor for parking slot 2
 IR_PARKING3_PIN = 25    # IR sensor for parking slot 3
-
-# LEDs for parking slot 1
-GREEN_LED_PARKING1_PIN = 5       # Green LED for parking slot 1 (available)
-RED_LED_PARKING1_PIN = 6         # Red LED for parking slot 1 (occupied)
-
-# LEDs for parking slot 2
-GREEN_LED_PARKING2_PIN = 13      # Green LED for parking slot 2 (available)
-RED_LED_PARKING2_PIN = 19        # Red LED for parking slot 2 (occupied)
-
-# LEDs for parking slot 3
-GREEN_LED_PARKING3_PIN = 26      # Green LED for parking slot 3 (available)
-RED_LED_PARKING3_PIN = 21        # Red LED for parking slot 3 (occupied)
 
 
 # Total number of parking slots
@@ -36,38 +35,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(IR_PARKING1_PIN, GPIO.IN)
 GPIO.setup(IR_PARKING2_PIN, GPIO.IN)
 GPIO.setup(IR_PARKING3_PIN, GPIO.IN)
-GPIO.setup(GREEN_LED_PARKING1_PIN, GPIO.OUT)
-GPIO.setup(RED_LED_PARKING1_PIN, GPIO.OUT)
-GPIO.setup(GREEN_LED_PARKING2_PIN, GPIO.OUT)
-GPIO.setup(RED_LED_PARKING2_PIN, GPIO.OUT)
-GPIO.setup(GREEN_LED_PARKING3_PIN, GPIO.OUT)
-GPIO.setup(RED_LED_PARKING3_PIN, GPIO.OUT)
-
-
-# Function to indicate parking spot status
-def update_led1(occupied):
-    if occupied:
-        GPIO.output(GREEN_LED_PARKING1_PIN, GPIO.LOW)  # Turn off green LED
-        GPIO.output(RED_LED_PARKING1_PIN, GPIO.HIGH)   # Turn on red LED (occupied)
-    else:
-        GPIO.output(GREEN_LED_PARKING1_PIN, GPIO.HIGH)  # Turn on green LED (empty)
-        GPIO.output(RED_LED_PARKING1_PIN, GPIO.LOW)    # Turn off red LED
-
-def update_led2(occupied):
-    if occupied:
-        GPIO.output(GREEN_LED_PARKING2_PIN, GPIO.LOW)  # Turn off green LED
-        GPIO.output(RED_LED_PARKING2_PIN, GPIO.HIGH)   # Turn on red LED (occupied)
-    else:
-        GPIO.output(GREEN_LED_PARKING2_PIN, GPIO.HIGH)  # Turn on green LED (empty)
-        GPIO.output(RED_LED_PARKING2_PIN, GPIO.LOW)    # Turn off red LED        
-
-def update_led3(occupied):
-    if occupied:
-        GPIO.output(GREEN_LED_PARKING3_PIN, GPIO.LOW)  # Turn off green LED
-        GPIO.output(RED_LED_PARKING3_PIN, GPIO.HIGH)   # Turn on red LED (occupied)
-    else:
-        GPIO.output(GREEN_LED_PARKING3_PIN, GPIO.HIGH)  # Turn on green LED (empty)
-        GPIO.output(RED_LED_PARKING3_PIN, GPIO.LOW)    # Turn off red LED
 
 
 # Function to count available parking slots
@@ -87,17 +54,31 @@ def count_available_slots():
         available_slots += 1
     
     return available_slots
+   
+# Function to update firebase
+def update_firebase(available_slots):
+    ref.set({
+        'Total_Slots': available_slots
+    })
 
 
 # Function to update the available parking slots on the LCD
 def update_lcd(available_slots, total_slots=3):
     lcd.clear()
+    lcd.move_to(3,0)
     lcd.putstr(f"Available: {available_slots}/{total_slots}")
 
+
 # Initialize the LCD display
-lcd.putstr("AVAILABLE PARKING")  # Initial message
+lcd.move_to(1,0)
+lcd.putstr("WELCOME TO PARKIT")
+lcd.move_to(6,1)
+lcd.putstr("PARKING")
 time.sleep(2)
 lcd.clear()
 
-available_slots = count_available_slots()
-update_lcd(available_slots)
+while True:
+    available_slots = count_available_slots()
+    update_lcd(available_slots)
+    update_firebase(available_slots)
+    time.sleep(1)
